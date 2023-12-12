e0afcf50b13f05bf5e4e69f28b6ab0978daa2ad2
guijp <guilherme93@gmail.com>
2023-12-12 13:22:06 +0100

    housekeeping

--- train_classifier.py
+++ train_classifier.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
-from dataset import WSIAEDataset
-from autoencoders import Transfer, SSIMLoss, Classifier
+from utils.model_definitions import Classifier, SSIMLoss
 import numpy as np
 from PIL import Image
 import torch
 import numpy as np
-from datasets import get_datasets
+from utils.datasets import get_datasets
 import sys
 
 # Settings
 sys.stdout = open(sys.stdout.fileno(), 'w', 1)
-loss_function_bce = torch.nn.BCELoss().cuda()
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+loss_function_bce = torch.nn.BCELoss().to(device)
 print("running on: {}".format(device))
 
 # Creating dataset
@@ -22,16 +21,17 @@ train_paths = [
                'Gleason/Data_0_SUS/20180911_Linkoping/train_299',
                'Gleason/Data_0_SUS/20180911_Rotterdam/train_299'
               ]
+
 tr_dataset, val_dataset = get_datasets(train_paths, device, val_split=0, binary=True)
-loader_trn = torch.utils.data.DataLoader(dataset=tr_dataset, batch_size=32, shuffle=True)
+loader_trn = torch.utils.data.DataLoader(dataset=tr_dataset, batch_size=2, shuffle=True)
 n_tr = len(tr_dataset)
 
 # Initializing classifier
-classifier = Classifier().cuda()
+classifier = Classifier().to(device)
 
 # Training
 optimizer = torch.optim.Adam(classifier.parameters(), lr = 1e-3, weight_decay = 1e-8)
-n_epochs = 1
+n_epochs = 10
 
 # Training model
 for epoch_i in range(n_epochs):
